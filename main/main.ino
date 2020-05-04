554a8334c21b81510c50f8d40cc348736592b794
AliMagid <31350830+AliMagid@users.noreply.github.com>
2020-05-04 23:34:24 +0200

    fuck jumpers

--- main/main.ino
+++ main/main.ino
@@ -1,40 +1,45 @@
-#include<ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <SoftwareSerial.h>
 
-#include <SoftwareSerial.h>
 SoftwareSerial EspSerial(2, 3); // RX, TX
 
-const char *ssid = "Esp-01"; // The name of the Wi-Fi network that will be created
-const char *password = "*smarthome*";
+const char *ssid       = "Esp-01"; // The name of the Wi-Fi network that will be created
+const char *password   = "*smarthome*";
+const char *myssid     = "E.A";
+const char *mypassword = "4208694*";
 
-IPAddress local_IP(192, 168, 1, 6);
-
+IPAddress local_IP(192, 168, 1, 17);
 IPAddress gateway(192, 168, 1, 1);
-
 IPAddress subnet(255, 255, 255, 0);
 
 ESP8266WebServer server(80);
-
 // Writing a simple HTML page.
-char HTML[] = "<html><body><h1>Inovo Smart Home</h1><h2>Created by Alolo & Alaa</h2><button><a href=\"LED\">Toggle LED</a></button> </body></html>"; // --> Simple HTML page
+char HTML[] = "<html><body><h1>Inovo Smart Home</h1><h2>Created by Alolo & Alaa</h2><button action=\"/LED\">Toggle LED</button> </body></html>";
 
 void setup() {
   pinMode(2, OUTPUT);
   Serial.begin(115200);
   Serial.setTimeout(3);
-  Serial.println();
-
+  
+//  WiFi.mode(WIFI_AP_STA);
+//  WiFi.begin(myssid,mypassword);
+//  
+//  while(WiFi.status()!=WL_CONNECTED)
+//  {
+//    Serial.print(".");
+//    delay(500);
+//  }
+//  Serial.println("");
+//  Serial.print("IP Address: ");
+//  Serial.println(WiFi.localIP());
+  
+  Serial.println("");
   Serial.print("Setting soft-AP configuration ... ");
-
   Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
-
   Serial.print("Setting soft-AP ... ");
-
   Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
-
-  Serial.print("Soft-AP IP address = ");
-
+  Serial.print("AP IP address = ");
   Serial.println(WiFi.softAPIP());
 
   server.on("/", handleRoot);
@@ -53,7 +58,8 @@ void loop() {
 void LED()
 {
   digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
-  server.send(200, "text/html",HTML);
+  server.send(204, "");
+  Serial.println("ToggleLed");
 }
 void handleRoot() {
   server.send(200, "text/html",HTML);

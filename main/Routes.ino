05a89bf1e78ed7aae2d2af984cccfc0653417fad
AliMagid <31350830+AliMagid@users.noreply.github.com>
2020-05-10 13:39:53 +0200

    reorder

--- main/Routes.ino
+++ main/Routes.ino
@@ -1,4 +1,6 @@
 void handleRoutes(){
+  SoftAPInit();
+  startMDNS();
   homePage();
   LED();
   tempHum();
--- main/SoftAPInit.ino
+++ main/SoftAPInit.ino
@@ -1,4 +1,8 @@
-void SoftAPInit(){
+void SoftAPInit() {
+  IPAddress local_IP(192, 168, 1, 17);
+  IPAddress gateway(192, 168, 1, 1);
+  IPAddress subnet(255, 255, 255, 0);
+
   Serial.println("");
   Serial.print("Setting soft-AP configuration ... ");
   Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
